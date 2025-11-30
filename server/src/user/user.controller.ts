859b5578c39257447182ed4876b1579171dc090c
Khoa-Neee <khoaitb7@gmail.com>
2025-11-30 15:57:19 +0700

    fix /user/register

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,10 +1,17 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Options, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Options('register')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  // Xử lý preflight CORS để tránh lỗi OPTIONS trên môi trường deploy
+  handleRegisterOptions() {
+    return;
+  }
 
   @Post('register')
   @HttpCode(HttpStatus.CREATED)
