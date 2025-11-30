c7e7d1cadf00805269bc01d149b463d93298d786
Khoa-Neee <khoaitb7@gmail.com>
2025-11-30 16:06:53 +0700

    fix

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import { NextFunction, Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,13 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
     preflightContinue: false,
+  });
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    // Trả về 204 cho mọi yêu cầu preflight để tránh lỗi 404 OPTIONS
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(204);
+    }
+    return next();
   });
   app.useGlobalPipes(
     new ValidationPipe({
