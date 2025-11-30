a36a56266dc0405320ef48a9642ffe789047f122
Khoa-Neee <khoaitb7@gmail.com>
2025-11-30 15:48:48 +0700

    fix

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,6 +9,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.FRONTEND_URL ?? 'http://localhost:5173',
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    preflightContinue: false,
   });
   app.useGlobalPipes(
     new ValidationPipe({
